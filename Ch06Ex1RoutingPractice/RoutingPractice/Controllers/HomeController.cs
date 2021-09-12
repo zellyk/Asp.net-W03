20a9693d4b9344323a6757447b416dc1eb6f5d88
jonathan ma <jonathan.marcotte.7@gmail.com>
2021-09-12 00:47:20 -0400

    Initial commit

---
+++ Ch06Ex1RoutingPractice/RoutingPractice/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoutingPractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Returns home so Index is set as the homepage.
+        public IActionResult Index()
+        {
+            return Content("Home");
+        }
+
+        public IActionResult Privacy()
+        {
+            return Content("Privacy");
+        }
+
+        public IActionResult Display(string id)
+        {
+            if (id == null)
+            {
+                return Content("No ID supplied.");
+            }
+            else
+            {
+                return Content("ID: " + id);
+            }
+        }
+
+        //Routes the URL so that when we load this site we run the application
+        // without typing the path /home/countdown
+        //giving conditions, such that we can decide where the loop starts and
+        //ends. The "?" symbol in the curly braces means that the segment of the
+        //URL is optional
+        [Route("[action]/{start}/{end?}/{message?}")]
+        //Loop that is a countdown and returns the output of the countdown 
+        // then prints the result on the page
+        public IActionResult Countdown(int start, int end = 0, string message="")
+        {
+            string contentString = "Counting down: \n"; // Assign value
+            for (int i = start; i >= end; i--) // Start loop
+            {
+                contentString += i + "\n"; // Appending value
+            }
+            contentString += message + "\n"; // prints the string if included
+            return Content(contentString); // Return message as a string
+        }
+    }
+}
---
+++ Ch06Ex1RoutingPractice/RoutingPractice/Startup.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RoutingPractice
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            // This is the default endpoint so the home page doesn't need /Index
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
